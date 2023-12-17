bea1e67fb3d7a3939310c1ffc101d64da109e70b
Colin Huber <cbhuber@gmail.com>
2023-12-16 18:45:43 -0700

    doc: Code comments.

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -7,6 +7,7 @@ const Banner = ({ name, childStyles, parentStyle }) => (
     >
       {name}
     </p>
+    {/* Circles in the banner via CSS */}
     <div className="absolute w-48 h-48 sm:w-32 sm:h-32 rounded-full -top-9 -left-16 -z-5 white-bg" />
     <div className="absolute w-72 h-72 sm:w-56 sm:h-56 rounded-full -bottom-24 -right-14 -z-5 white-bg" />
   </div>
