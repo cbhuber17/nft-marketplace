19b2744ca1942c0f750889b42faef09cfebae3f7
Colin Huber <cbhuber@gmail.com>
2023-12-22 21:41:17 -0700

    env: Hardhat config update for Sepolia deployment.

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,22 +1,16 @@
-const fs = require("fs");
-
-require("@nomiclabs/hardhat-waffle");
-
-const privateKey = fs.readFileSync(".secret").toString().trim();
+// require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   networks: {
     hardhat: {
       chainId: 1337,
     },
-    // mumbai: {
-    //   url: 'https://rpc-mumbai.maticvigil.com',
-    //   accounts: [privateKey],
-    // },
-    // rinkeby: {
-    // url: 'https://rinkeby.infura.io/v3/bed4fdcc76bb4978a9a3103ef0946f64',
-    //   accounts: [privateKey],
-    // },
+    sepolia: {
+      url: process.env.INFURIA_RPC_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY],
+    },
   },
   solidity: "0.8.4",
 };
