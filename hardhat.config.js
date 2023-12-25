6d3f7e2e16b0a81110e3f8da35ceba4147a8cbe0
Colin Huber <cbhuber@gmail.com>
2023-12-24 20:59:43 -0700

    fix: hexidecimal notation.

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,7 +9,7 @@ module.exports = {
     },
     sepolia: {
       url: process.env.NEXT_PUBLIC_INFURIA_RPC_URL,
-      accounts: [process.env.NEXT_PUBLIC_SEPOLIA_PRIVATE_KEY],
+      accounts: [`0x${process.env.NEXT_PUBLIC_SEPOLIA_PRIVATE_KEY}`],
     },
   },
   solidity: "0.8.4",
